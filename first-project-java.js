69b9a9d5476b1e83d129f7fe7509c70ee6a9cae3
landon-is-on <landonsmothers24@gmail.com>
2021-04-19 18:20:24 -0400

    added mark through

--- first-project-java.js
+++ first-project-java.js
@@ -3,8 +3,15 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click',function() {
-var paragraph = document.createElement('p')
-paragraph.classList.add('paragraph-styling');
-paragraph.innerText = inputField.value; 
-toDoContainer.appendChild(paragraph);
+    let paragraph = document.createElement('p')
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value; 
+    toDoContainer.appendChild(paragraph);
+    inputField.value = -- 
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dbclick',function() {
+        toDoContainer.removeChild(paragraph);
+    })
 })
